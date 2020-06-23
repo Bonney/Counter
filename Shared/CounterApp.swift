34b93fa1820aed629cd714827635f8695bbb4abe
Matt Bonney <bonneym@gmail.com>
2020-06-23 08:33:59 -0400

    Initial Commit

---
+++ Shared/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Shared
+//
+//  Created by Matt Bonney on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
